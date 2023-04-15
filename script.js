6d2db73eba172572681e6265f00fac83c6267941
kevindinh3 <dinhkevin1@gmail.com>
2023-04-14 20:09:57 -0400

    first 2 use cases

---
+++ script.js
@@ -0,0 +1,28 @@
+var a = 0;
+var b = 0;
+var operator = '';
+
+const add = function(a, b) {
+    return a + b;
+};
+console.log('Add', add(a,b));
+
+
+const subtract = function(a, b) {
+    return a - b;
+};
+console.log('Subtract', subtract(a,b));
+
+
+const multiply = function(a, b) {
+    return a * b;
+}
+console.log('Multiply', multiply(a,b));
+
+
+const divide = function(a, b) {
+    return a / b;
+};
+console.log('Divide', divide(a, b));
+
+
