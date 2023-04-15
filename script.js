7e94720b1e62ecebcd100cfbe1b5717263ebc2ce
kevindinh3 <dinhkevin1@gmail.com>
2023-04-14 20:32:32 -0400

    created the operate function

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-var a = 0;
-var b = 0;
+let a = 0;
+let b = 0;
 var operator = '';
 
 const add = function(a, b) {
     return a + b;
 };
-console.log('Add', add(a,b));
-
 
 const subtract = function(a, b) {
     return a - b;
 };
-console.log('Subtract', subtract(a,b));
-
 
 const multiply = function(a, b) {
     return a * b;
-}
-console.log('Multiply', multiply(a,b));
-
+};
 
 const divide = function(a, b) {
     return a / b;
 };
-console.log('Divide', divide(a, b));
 
-
+const operate = function(a, b, operator) {
+    
+    switch(operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+    }
+}
+console.log(operate(5,10,'+'));
